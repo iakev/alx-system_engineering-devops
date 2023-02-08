7ba9a6011192791b17d1a288ce7b44155b470875
iakev <kirimikmwongera@gmail.com>
2023-02-08 10:07:40 +0000

    added a script that exports data to a csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A module consuming a REST API and exporting data of interest
+to a CSV file
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    csv_file = "USER_ID.csv"
+    # get the user data from users api
+    employee_id = sys.argv[1]
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                     .format(employee_id))
+    user = r.json()
+    # get todo list associated with the user
+    payload = {'userId': employee_id}
+    r = requests.get("https://jsonplaceholder.typicode.com/todos",
+                     params=payload)
+    todos = r.json()
+    with open(csv_file, 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([todo.get('userId'), user.get('username'),
+                             todo.get('completed'), todo.get('title')])
