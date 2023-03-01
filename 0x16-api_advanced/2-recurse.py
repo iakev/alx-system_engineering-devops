dd8e1a5e9ead487975fa655257946f8b2edb3043
iakev <kirimikmwongera@gmil.com>
2023-03-01 16:52:12 +0000

    modified to check for status code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,22 +10,23 @@ def recurse(subreddit, hot_list=[], count=0, payload={}):
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
     r = requests.get(url, headers=headers, params=payload,
                      allow_redirects=False)
-    full_data = r.json()
-    data = full_data.get('data')
-    if data:
-        after = data.get('after')
-        children = data.get('children')
-        if children:
-            count = len(children)
-            for child in children:
-                child_data = child.get('data')
-                if child_data:
-                    hot_list.append(child_data.get('title'))
-            if after is not None:
-                payload['after'] = after
-                hot_list = recurse(subreddit, hot_list, count, payload)
-            else:
-                return hot_list
+    if r.status_code == 200:
+        full_data = r.json()
+        data = full_data.get('data')
+        if data:
+            after = data.get('after')
+            children = data.get('children')
+            if children:
+                count = len(children)
+                for child in children:
+                    child_data = child.get('data')
+                    if child_data:
+                        hot_list.append(child_data.get('title'))
+                if after is not None:
+                    payload['after'] = after
+                    hot_list = recurse(subreddit, hot_list, count, payload)
+                else:
+                    return hot_list
     else:
         return None
     return hot_list
