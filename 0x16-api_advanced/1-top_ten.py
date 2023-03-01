fc1ce0c03269772d7b5c452ae1164232b7a71a19
iakev <kirimikmwongera@gmil.com>
2023-03-01 16:42:50 +0000

    modified to check for status code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,16 +11,20 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
     r = requests.get(url, headers=headers, params=payload,
                      allow_redirects=False)
-    full_data = r.json()
-    data = full_data.get('data')
-    if data:
-        children = data.get('children')
-        if children:
-            for child in children:
-                child_data = child.get('data')
-                if child_data:
-                    print(child_data.get('title'))
+    data = None
+    if r.status_code == 200:
+        full_data = r.json()
+        data = full_data.get('data')
+        if data:
+            children = data.get('children')
+            if children:
+                for child in children:
+                    child_data = child.get('data')
+                    if child_data:
+                        print(child_data.get('title'))
+            else:
+                print(children)
         else:
-            print(children)
+            print(data)
     else:
         print(data)
