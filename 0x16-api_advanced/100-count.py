0dd444884bb5a888e0cb820ef1b37ed970e9242d
iakev <kirimikmwongera@gmil.com>
2023-03-01 17:49:07 +0000

    modified to check for status code

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,39 +14,40 @@ def count_words(subreddit, word_list,
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
     r = requests.get(url, headers=headers, params=payload,
                      allow_redirects=False)
-    try:
-        full_data = r.json()
-    except Exception as e:
-        return
-    data = full_data.get('data')
-    if data:
-        after = data.get('after')
-        children = data.get('children')
-        if children:
-            count = len(children)
-            for child in children:
-                child_data = child.get('data')
-                if child_data:
-                    title = child_data.get('title')
-                    title_list = title.split()
-                    for key_word in word_list:
-                        for word in title_list:
-                            if key_word.lower() == word.lower():
-                                word_count[key_word.lower()] += 1
-            if after is not None:
-                payload['after'] = after
-                word_count, recurse_done = count_words(subreddit, word_list,
+    if r.status_code == 200:
+        try:
+            full_data = r.json()
+        except Exception as e:
+            return
+        data = full_data.get('data')
+        if data:
+            after = data.get('after')
+            children = data.get('children')
+            if children:
+                count = len(children)
+                for child in children:
+                    child_data = child.get('data')
+                    if child_data:
+                        title = child_data.get('title')
+                        title_list = title.split()
+                        for key_word in word_list:
+                            for word in title_list:
+                                if key_word.lower() == word.lower():
+                                    word_count[key_word.lower()] += 1
+                if after is not None:
+                    payload['after'] = after
+                    word_count, recurse_done = count_words(subreddit, word_list,
                                                        count, payload,
                                                        word_count,
                                                        recurse_done)
-            else:
-                return word_count, True
-    else:
-        return None
-    if recurse_done:
-        sorted_count = dict(sorted(word_count.items(),
-                                   key=lambda item: item[0]))
-        for k, v in sorted_count.items():
-            if v > 0:
-                print("{}: {}".format(k, v))
-    return word_count, False
+                else:
+                    return word_count, True
+        else:
+            return None
+        if recurse_done:
+            sorted_count = dict(sorted(word_count.items(),
+                                       key=lambda item: item[0]))
+            for k, v in sorted_count.items():
+                if v > 0:
+                    print("{}: {}".format(k, v))
+        return word_count, False
