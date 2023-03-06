b3bb65ba2934bf07014c7fb8447d175a100a1da1
iakev <kirimikmwongera@gmil.com>
2023-03-06 12:15:03 +0000

    modifed to pass puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # A puppet manifessts that fixes a lamp wordpress webstack
 exec { 'fix-wordpress':
-     command => "/bin/sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
+  command => "/bin/sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
 }
